8d3e9c71443323702bf4d10559283b8935981dba
Dakshitha Dissanayake <ddissnayake@virtusa.com>
2018-04-02 15:19:56 +0530

    added gitignore

--- service-stock/bin/default/com/test/repo/CustomerRepository.groovy
+++
@@ -1,9 +0,0 @@
-package com.test.repo
-
-import com.test.common.repo.BaseRepository
-import com.test.model.Customer
-
-interface CustomerRepository extends BaseRepository<Customer> {
-
-	Customer findByName(String firstName)
-}
